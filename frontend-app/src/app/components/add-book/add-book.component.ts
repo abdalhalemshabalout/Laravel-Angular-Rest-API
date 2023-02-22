d7d9896a78af3f68ea94716bd99b27fe7929ac35
abdalhalemshabalout <halimsamoglu136@gmail.com>
2023-02-22 21:02:15 +0300

    added form and controller have been completed

--- frontend-app/src/app/components/add-book/add-book.component.ts
+++ frontend-app/src/app/components/add-book/add-book.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, NgZone} from '@angular/core';
+import {Route, Router} from '@angular/router';
+import { from } from 'rxjs';
+import {CrudService} from '../../service/crud.service';
+import {FormGroup, FormBuilder} from '@angular/forms';
 @Component({
   selector: 'app-add-book',
   templateUrl: './add-book.component.html',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddBookComponent implements OnInit {
 
-  constructor() { }
+  bookForm: FormGroup;
+
+  constructor(
+    public formBuilder:FormBuilder,
+    private router:Router,
+    private ngZone:NgZone,
+    private crudService: CrudService
+  ) 
+  { 
+    this.bookForm= this.formBuilder.group({
+      name:[''],
+      price:[''],
+      description:[''],
+    })
+  }
 
   ngOnInit(): void {
   }
 
+  onSubmit():any{
+    this.crudService.addBook(this.bookForm.value)
+    .subscribe(()=>{
+      console.log('Dat added successfuly');
+      this.ngZone.run(()=>this.router.navigateByUrl('/books-list'))
+    }, (err)=>{
+      console.log(err);
+    })
+  }
 }
